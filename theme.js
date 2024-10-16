67b6beda0ee1dd0d739b03e28c5887fb10663b0f
thatguystephen <119560893+thatguystephen@users.noreply.github.com>
2024-10-15 23:48:24 -0500

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,9 @@
+(() => {
+    const script = document.createElement("SCRIPT");
+    script.setAttribute("type", "text/javascript");
+    script.setAttribute(
+      "src",
+      "C:\Users\Steph\GitHub\Hazy\hazy.js"
+    );
+    document.head.appendChild(script);
+  })();
